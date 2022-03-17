76be5cd2bbcd73e4703591f3a311589ca3ed958f
a806955 <99879612+a806955@users.noreply.github.com>
2022-03-17 11:42:52 +0100

    Update practica2_6.sh

--- practica2_6.sh
+++ practica2_6.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
-#800616, Beltran Pardos, Miguel, T, 1, B
+#800616, Beltran Pardos, Miguel, T, 1, B??????
+#El panita Jan
 
-
-dir=bin223
-
-if test $(ls /home/"$USER" -r -l -t | grep '^d' | grep 'bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | wc -l) -eq 0
-then
-	echo "Se ha creado el directorio /home/$USER/$dir"
-	mkdir /home/"$USER"/"$dir"
+if test $(ls /home/"$USER" -l -t | grep '^d' | grep 'bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | wc -l) -eq 0 
+then 
+	dir=$(mktemp -d /home/"$USER"/binXXX)
+  echo "esto es el FUCNGIIGAIJSDAJSD dir ->>>>>>>>>>>>>>>>> $dir"
+  echo "Se ha creado el directorio $dir"
 else
-	dir=$(ls /home/"$USER" -r -l -t | grep '^d' | grep 'bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | cut -f10 -d" " | head -n1)
+	dir=$(ls /home/"$USER" -r -l -t | grep '^d' | grep 'bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | cut -f13 -d" " | head -n1)
 fi
 
-echo "Directorio destino copia: /home/$USER/$dir"
+echo "Directorio destino de copia: /home/$USER/$dir"
 
 total=0
 for i in $(ls) #cualquier cosa que no sea un dir y sea ejecutable
 do
 	if [ -x "$i" -a ! -d "$i" ]
 	then
-		echo "./$i ha sido copiado a /home/$USER/$dir"
-		cp "$i" "/home/$USER/$dir"
+		cp "$i" "$dir"
+		echo "./$i ha sido copiado a $dir"
 		total=$(($total + 1))
 	fi
 done
 
-if test $total -gt 0
+if test $total -gt 0 
 then
 	echo "Se han copiado $total archivos"
 else
